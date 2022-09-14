93736cce3e8fc794170f79a22bb07aa1bb6934fc
unknown <deshmukh.prateek@gmail.com>
2022-09-14 05:30:22 +0000

    first commit

---
+++ Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shopping.Client.Models
+{
+    
+    //https://tinyurl.com/5xk5jfu2
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public string ImageFile { get; set; }
+        public decimal Price { get; set; }
+    }
+}
